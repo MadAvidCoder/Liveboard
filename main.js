3404b9fbac1e824ea452dd1fc2b208d7269e4427
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-09-18 23:54:34 +1000

    Implement rudimentary canvas
    Created a basic canvas where you can draw and clear your drawings. You can zoom and also pan.

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const isDev = require("electron-is-dev");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-function createWindow() {
+function createWindow () {
   const win = new BrowserWindow({
-    width: 1024,
-    height: 768,
+    width: 1200,
+    height: 800,
     webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-    },
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
   });
 
-  win.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "build/index.html")}`
-  );
+  const isDev = !app.isPackaged;
+
+  if (isDev) {
+    win.loadURL('http://localhost:3000');
+  } else {
+    win.loadFile(path.join(__dirname, 'index.html'));
+  }
 }
 
 app.whenReady().then(createWindow);
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
